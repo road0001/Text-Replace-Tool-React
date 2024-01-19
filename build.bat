d2e8c01429e0558276dfaf8b2a6845a7a939305f
JMRY <376509849@qq.com>
2024-01-19 17:41:30 +0800

    优化打包脚本。

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
+rmdir /S /Q build
+rmdir /S /Q "Text-Replace-Tool-React\static"
+del /Q "Text-Replace-Tool-React\debug.log"
 call npm run build
 xcopy /E /Y build\* Text-Replace-Tool-React\
 pause
